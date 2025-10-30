4b9dd1cf1896c5bfd9e47d6785f57c2e3f4ee61b
Kian Fitzpatrick <kian.fitzpatrick2000@gmail.com>
2025-10-30 01:39:17 +0000

    Shared/Util: Created basic util types

---
+++ src/Shared/Utils/RGB.h
@@ -0,0 +1,13 @@
+#ifndef __H_RGB
+#define __H_RGB
+
+#include <cstdint>
+
+struct RGB
+{
+	uint8_t R;
+	uint8_t G;
+	uint8_t B;
+};
+
+#endif
---
+++ src/Shared/Utils/Vec2.h
@@ -0,0 +1,14 @@
+#include <cstdint>
+#ifndef __H_VEC2
+#define __H_VEC2
+
+class Vec2 {
+
+public:
+	float x;
+	float y;
+
+};
+
+
+#endif
